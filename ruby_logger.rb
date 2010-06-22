ae391983d93379f415f1012c42cd81617a0da141
David M. Rosenberg <dmr@davidrosenberg.me>
2010-06-21 22:29:59 -0500

    Initial commit

---
+++ ruby_logger.rb
@@ -0,0 +1,86 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+# ruby_logger.rb
+
+
+class LoggerException < StandardError
+end
+
+class LoggerDepthException < LoggerException
+end
+
+
+module Logger
+  attr :log_depth
+  attr :log_logfile
+  attr :log_flags
+  attr :log_stream
+  
+  def log_init(logfile=nil, logflags=[])
+    @log_depth   = 0
+    @log_logfile = logfile
+    @log_flags   = []
+    if logflags.member? :Log
+      @log_flags << :Log
+    end
+    if logflags.member? :Debug
+      @log_flags << :Debug
+    end
+    @log_stream  = nil
+
+  end
+
+  def log_depth_inc
+    @log_depth += 1
+  end
+
+  def log_depth_dec
+    if @log_depth > 0
+      @log_depth -= 1
+    else
+      raise(LoggerDepthException, "Cannot decrease log depth below zero")
+    end
+  end
+
+  def log_openstream
+    if !(@log_flags.member? :StreamOpen)
+      @log_stream = File.open(@log_logfile, "a")
+      @log_flags << :StreamOpen
+    else
+      log_mesg('Attempting to open already open logstream', :Warning)
+    end
+  end
+
+  def log_closestream
+    if @log_flags.member? :StreamOpen
+      @log_stream.close
+      @log_stream = nil
+      @log_flags.delete :StreamOpen
+    else
+      log_mesg('Attempting to close an unopened or already closed logstream', :Warning)
+    end
+  end
+
+  def log_mesg(mesg, level=:Debug)
+    mesg_content = ""
+    for ii in 1..@log_depth
+      mesg_content += "  "
+    end
+
+    mesg_content += (level.to_s + ": " + mesg)
+
+    if @log_flags.member? :Debug
+      puts mesg_content
+    end
+
+    if @log_flags.member? :Log
+      if @log_flags.member? :StreamOpen
+        @log_stream.puts mesg_content
+      else
+        log_openstream
+        @log_stream.puts mesg_content
+        log_closestream
+      end
+    end
+  end
+end
---
+++ ruby_testclass.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+# ruby_testclass.rb
+
+require "ruby_logger"
+
+class TestMe
+  include Logger
+  
+  def initialize(logfile, log_flags=[])
+    @nums   = [1,1,2,3,5]
+    @primes = []
+    @max_prime = 1000
+
+    log_init(logfile, log_flags)
+  end
+
+  def count_nums
+    log_depth_inc
+    log_mesg("Counting nums", :Debug)
+    
+    log_depth_dec
+    return @nums.length
+  end
+
+  def add_prime(n)
+    log_depth_inc
+    log_mesg("Attempting to add #{n.to_s}")
+    if is_prime(n)
+      @primes << n
+      log_mesg("Adding #{n.to_s}", :Warning)
+    else
+      log_mesg("Not adding #{n.to_s}", :Warning)
+    end
+    log_depth_dec
+  end
+
+  def is_prime(n)
+    log_depth_inc
+    log_mesg("Determining if #{n.to_s} is prime", :Debug)
+    res = nil
+    if [1,2,3,5,7,9,11,13,17,19,21,23].member? n
+      res = 1
+      log_mesg("It is")
+    else
+      log_mesg("It is not")
+      res = nil
+    end
+    log_depth_dec
+    return res
+  end
+
+end
